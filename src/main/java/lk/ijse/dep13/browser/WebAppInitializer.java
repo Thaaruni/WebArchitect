5d0975f8d0df8e328d566d7f337f3f7f0f55f9c9
Thaaruni Dissanayake <tharunidissanayake99@gmail.com>
2025-02-22 18:43:56 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/dep13/browser/WebAppInitializer.java
@@ -0,0 +1,16 @@
+package lk.ijse.dep13.browser;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class WebAppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
---
+++ src/main/java/lk/ijse/dep13/browser/controller/MainSceneController.java
@@ -0,0 +1,5 @@
+package lk.ijse.dep13.browser.controller;
+
+public class MainSceneController {
+
+}

ae118d3edc892dba5ce07d7714c3083e907510e1
Thaaruni Dissanayake <tharunidissanayake99@gmail.com>
2025-02-24 07:24:53 +0530

    Final Commit: Complete Implementation of WebArchitect

--- src/main/java/lk/ijse/dep13/browser/controller/MainSceneController.java
+++ src/main/java/lk/ijse/dep13/browser/controller/MainSceneController.java
@@ -7,7 +7,14 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainSceneController {
     public WebView wbDisplay;
@@ -75,9 +82,104 @@ public class MainSceneController {
             Socket socket = new Socket(host, port);
             String baseUrl = protocol + "://" + host + ":" + port + "/";
 
+            // Read the server response
+            new Thread(() -> {
+                try {
+                    InputStream is = socket.getInputStream();
+                    InputStreamReader isr = new InputStreamReader(is);
+                    BufferedReader bsr = new BufferedReader(isr);
 
-        } catch (Exception e) {
-            e.printStackTrace();
+                    // Let's read the status line
+                    String statusLine = bsr.readLine();
+                    int statusCode = Integer.parseInt(statusLine.split(" ")[1]);
+                    boolean redirection = statusCode >= 300 && statusCode < 400;
+
+                    // Let's read the request headers
+                    String line;
+                    String contentType = null;
+                    boolean chunked = false;
+
+                    while ((line = bsr.readLine()) != null && !line.isBlank()) {
+                        String header = line.split(":")[0].strip();
+                        String value = line.substring(line.indexOf(":") + 1).strip();
+                        if (redirection) {
+                            if (!header.equalsIgnoreCase("Location")) continue;
+                            System.out.println("Redirection: " + value);
+                            Platform.runLater(() -> txtAddress.setText(value));
+                            loadWebPage(value);
+                            return;
+                        } else {
+                            if (header.equalsIgnoreCase("Content-Type")) {
+                                contentType = value;
+                            } else if (header.equalsIgnoreCase("Transfer-Encoding")) {
+                                chunked = value.equalsIgnoreCase("chunked");
+                            }
+                        }
+                    }
+
+                    if (contentType == null || !contentType.contains("text/html")) {
+                        displayError("Sorry, content type not supported");
+                    } else {
+                        if (chunked) bsr.readLine();    // Skip the chunk size
+                        StringBuilder sb = new StringBuilder();
+                        while ((line = bsr.readLine()) != null) {
+                            sb.append(line);
+                        }
+                        if (chunked) sb.deleteCharAt(sb.length() - 1); // Delete the chunk boundary
+
+                        // Let's fix the relative url issue with the base url
+                        if (!Pattern.compile("<head>.*<base .*</head>").matcher(sb).find()) {
+                            Matcher headMatcher = Pattern.compile("<head>").matcher(sb);
+                            if (headMatcher.find()) {
+                                sb.insert(headMatcher.end(), "<base href='%s'>".formatted(baseUrl));
+                            }
+                        }
+
+                        // Let's set the title
+                        Matcher titleMatcher = Pattern.compile("<title>(.+)</title>", Pattern.CASE_INSENSITIVE).matcher(sb);
+                        if (titleMatcher.find()) {
+                            Platform.runLater(() -> {
+                                ((Stage) (root.getScene().getWindow())).setTitle("DEP Browser - " + titleMatcher.group(1));
+                            });
+                        }
+
+
+                        Platform.runLater(() -> wbDisplay.getEngine().loadContent(sb.toString()));
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    displayError("Connection Failed");
+                }
+            }).start();
+
+            // Send the client request
+            String httpRequest = """
+                    GET %s HTTP/1.1
+                    Host: %s
+                    User-Agent: Mozilla/5.0
+                    Connection: close
+                    Accept: text/html
+                    
+                    """.formatted(path, host);
+            socket.getOutputStream().write(httpRequest.getBytes());
+            socket.getOutputStream().flush();
+        } catch (RuntimeException | UnknownHostException e) {
+            displayError("400 Bad Request: Invalid URL");
+        } catch (IOException e) {
+            displayError("Connection Error");
         }
+    }
+
+    private void displayError(String message) {
+        Platform.runLater(() -> {
+            wbDisplay.getEngine().loadContent("""
+                    <!DOCTYPE html>
+                    <html>
+                    <body>
+                    <h1 style="text-align: center;">%s</h1>
+                    </body>
+                    </html>
+                    """.formatted(message));
+        });
     }
 }
